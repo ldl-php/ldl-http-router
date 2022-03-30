39c61dbb445b414fdd7cdeb4e86e5762e3b511b4
Federico Stange <jpfstange@gmail.com>
2022-03-30 11:32:27 +0000

    fix/1202049026119589 - Fix HttpCollection into usable state

--- src/LDL/Router/Core/Route/Collection/Traits/RouteCollectionInterfaceTrait.php
+++ src/LDL/Router/Core/Route/Collection/Traits/RouteCollectionInterfaceTrait.php
@@ -4,76 +4,27 @@ declare(strict_types=1);
 
 namespace LDL\Router\Core\Route\Collection\Traits;
 
-use LDL\Framework\Base\Collection\Contracts\CollectionInterface;
-use LDL\Router\Core\Route\Collection\RouteCollectionInterface;
-use LDL\Router\Core\Route\Group\RouteGroupInterface;
-use LDL\Router\Core\Route\Path\Result\RoutePathParsingResultInterface;
-use LDL\Router\Core\Route\Path\RoutePathParserInterface;
-use LDL\Router\Core\Route\RouteInterface;
+use LDL\Validators\Chain\ValidatorChainInterface;
 
 trait RouteCollectionInterfaceTrait
 {
     /**
-     * @var RoutePathParserInterface
+     * @var string
      */
-    private $pathParser;
+    private $_tRouteCollectionPath;
 
-    public function getPathParser(): RoutePathParserInterface
+    /**
+     * @var ValidatorChainInterface
+     */
+    private $_tRouteCollectionValidatorChain;
+
+    public function getPath(): string
     {
-        return $this->pathParser;
+        return $this->_tRouteCollectionPath;
     }
 
-    public function append($route, $key = null): CollectionInterface
+    public function getValidatorChain(): ValidatorChainInterface
     {
-        if ($route instanceof RouteGroupInterface) {
-            return $this->addGroup($route);
-        }
-
-        if ($route instanceof RouteInterface) {
-            return $this->addRoute($route);
-        }
-
-        return parent::append($route, $key);
-    }
-
-    public function addRoute(RouteInterface $route): RouteCollectionInterface
-    {
-        $result = $this->pathParser->parse($route, null, $route->getPath());
-        parent::append($result, $result->getPath());
-
-        return $this;
-    }
-
-    public function addGroup(RouteGroupInterface $group): RouteCollectionInterface
-    {
-        /**
-         * @var RouteInterface $route
-         */
-        foreach ($group as $route) {
-            $result = $this->pathParser->parse(
-                $route,
-                $group->getValidatorChain(),
-                $group->getPath(),
-                $route->getPath()
-            );
-
-            parent::append($result, $result->getPath());
-        }
-
-        return $this;
-    }
-
-    public function filterStaticRoutes(): RouteCollectionInterface
-    {
-        return $this->filter(static function (RoutePathParsingResultInterface $path) {
-            return !$path->isDynamic();
-        });
-    }
-
-    public function filterDynamicRoutes(): RouteCollectionInterface
-    {
-        return $this->filter(static function (RoutePathParsingResultInterface $path) {
-            return $path->isDynamic();
-        });
+        return $this->_tRouteCollectionValidatorChain;
     }
 }
--- src/LDL/Router/Http/Route/Collection/HttpRouteCollection.php
+++ src/LDL/Router/Http/Route/Collection/HttpRouteCollection.php
@@ -5,8 +5,32 @@ declare(strict_types=1);
 namespace LDL\Router\Http\Route\Collection;
 
 use LDL\Router\Core\Route\Collection\Traits\RouteCollectionInterfaceTrait;
+use LDL\Router\Http\Route\HttpRouteInterface;
+use LDL\Type\Collection\AbstractTypedCollection;
+use LDL\Validators\Chain\AndValidatorChain;
+use LDL\Validators\Chain\OrValidatorChain;
+use LDL\Validators\Chain\ValidatorChainInterface;
+use LDL\Validators\InterfaceComplianceValidator;
 
-class HttpRouteCollection
+class HttpRouteCollection extends AbstractTypedCollection implements HttpRouteCollectionInterface
 {
     use RouteCollectionInterfaceTrait;
+
+    public function __construct(
+        iterable $items = null,
+        string $path = '',
+        ValidatorChainInterface $chain = null
+    ) {
+        $this->_tRouteCollectionPath = $path;
+
+        $this->_tRouteCollectionValidatorChain = $chain ?? new AndValidatorChain();
+
+        $this->getAppendValueValidatorChain(OrValidatorChain::class)
+            ->getChainItems()
+            ->append(new InterfaceComplianceValidator(HttpRouteInterface::class))
+            ->append(new InterfaceComplianceValidator(HttpRouteCollectionInterface::class))
+            ->lock();
+
+        parent::__construct($items);
+    }
 }
