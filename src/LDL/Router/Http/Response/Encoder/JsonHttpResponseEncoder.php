e7fd2398536e039cfc68fb8a77df7713f7e5a6a7
Federico Stange <jpfstange@gmail.com>
2022-04-28 13:02:00 +0000

    feature/1202196953869449 - Add nameable interface + describable interface to HttpResponseEncoder

--- src/LDL/Router/Http/Response/Encoder/JsonHttpResponseEncoder.php
+++ src/LDL/Router/Http/Response/Encoder/JsonHttpResponseEncoder.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace LDL\Router\Http\Response\Encoder;
 
+use LDL\Framework\Base\Traits\DescribableInterfaceTrait;
+use LDL\Framework\Base\Traits\NameableTrait;
 use LDL\Http\Core\Response\ResponseInterface;
 use LDL\Router\Core\Route\Dispatcher\Result\Collection\RouteDispatcherResultCollectionInterface;
 use LDL\Router\Core\Route\Dispatcher\Result\RouteDispatcherResultInterface;
 
 class JsonHttpResponseEncoder implements HttpResponseEncoderInterface
 {
+    use NameableTrait;
+    use DescribableInterfaceTrait;
+
+    private const NAME = 'ldl-json';
+    private const DESC = 'Encodes dispatcher contents as JSON';
+
     /**
      * @var bool
      */
@@ -18,6 +26,8 @@ class JsonHttpResponseEncoder implements HttpResponseEncoderInterface
     public function __construct(bool $pretty = false)
     {
         $this->pretty = $pretty;
+        $this->_tName = self::NAME;
+        $this->_tDescription = self::DESC;
     }
 
     public function encode(ResponseInterface $response, RouteDispatcherResultCollectionInterface $result): void
